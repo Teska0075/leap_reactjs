750bd132ac023eb65b8e3d4462d92e6949ab6e59
Evening <evening@Admins-iMac.local>
2023-01-03 18:49:46 +0800

    commit

---
+++ firstreact/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+import NavigationBar from './components/header/navigation/NavigationBar';
+
+function App() {
+  const users = [
+  {name: ' Boldo', age: 33, imageURL: 'https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80'}, 
+  {name: ' Bataa', age: 28, imageURL: 'https://images.unsplash.com/photo-1541576980233-97577392db9a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1784&q=80'}, 
+  {name: ' Naraa', age: 34, imageURL: 'https://images.unsplash.com/photo-1505866535066-ccebd6b2b98a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1789&q=80'},
+  {name: ' Saraa', age: 24, imageURL: 'https://images.unsplash.com/photo-1534119768988-c496213eff76?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80'},
+  {name: ' Bulgaa', age: 20, imageURL: 'https://images.unsplash.com/photo-1534126416832-a88fdf2911c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80'}
+  ];
+
+  let sum = 0;
+  for(let i = 0; i < users.length; i++){
+    sum += users[i].age
+  }
+
+  let ave = sum/users.length;
+  
+  
+
+  return (
+    <div className="App">
+      <NavigationBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ firstreact/src/components/header/navigation/NavigationBar.js
@@ -0,0 +1,61 @@
+import css from '../../all.module.css/NavigationBar.css'
+
+const NavItems = [
+    {
+        name: "PORTFOLIO",
+        link: "#",
+        class: "NavItem"
+    },
+    {
+        name: "BLOG",
+        link: "#",
+        class: "NavItem"
+    },
+    {
+        name: "CV",
+        link: "#",
+        class: "NavItem"
+    },
+    {
+        name: "STORE",
+        link: "#",
+        class: "NavItem"
+    },
+    {
+        name: "FREELANCE",
+        link: "#",
+        class: "NavItem"
+    },
+    {
+        name: "ABOUT ME",
+        link: "#",
+        class: "NavItem"
+    },
+    {
+        name: "CONTACT",
+        link: "#",
+        class: "NavItem"
+    }
+];
+
+const NavigationBar = () => {
+    return (
+        <div className='Navbar'>
+            <div className="LeftSection">
+                <img className="Logo" src="/images/Leap.png" alt="logo"></img>
+                <button className="HireMe"><img className='img' src='https://www.svgrepo.com/show/108614/linkedin.svg'/>Hire Me</button>
+            </div>
+            <div className="NavMenu">
+                {
+                    NavItems.map((item) => {
+                        return (
+                            <a href={item.link} className={item.class}>{item.name}</a>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+};
+
+export default NavigationBar;
